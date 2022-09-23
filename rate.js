09401ee02e6cb1cef4792df75b68a9f23ad41fdc
rumeysa zeynep kabadayı <rumeysazeynepkabadayi@hotmail.com>
2022-09-23 22:36:42 +0300

    third commit

---
+++ rate.js
@@ -0,0 +1,4 @@
+document.querySelector("#btn_submit").addEventListener("click", function () {
+  document.querySelector("#thanks-box").classList.add("active");
+  document.querySelector("#rate-box").classList.remove("show");
+});
