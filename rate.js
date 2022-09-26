579b46cfa1399a8e62f29be03a13ce377d45c7d4
rumeysa zeynep kabadayı <rumeysazeynepkabadayi@hotmail.com>
2022-09-26 21:06:51 +0300

    final commit

--- rate.js
+++ rate.js
@@ -1,6 +1,11 @@
-let btn_submit = document.querySelector("#btn_submit");
-let thanks_box = document.querySelector("#thanks_box");
-let numbers = document.querySelector(".numbers");
+const btn_submit = document.querySelector("#btn_submit");
+const thanks_box = document.querySelector("#thanks_box");
+const numbers = document.querySelector(".numbers");
+const btn_1 = document.getElementById("btn1");
+const btn_2 = document.getElementById("btn2");
+const btn_3 = document.getElementById("btn3");
+const btn_4 = document.getElementById("btn4");
+const btn_5 = document.getElementById("btn5");
 
 let score;
 
@@ -11,58 +16,64 @@ function disabled() {
   btn_submit.classList.remove("disabled");
 }
 
-document.querySelector("#btn1").onclick = function () {
-  document.querySelector("#btn1").style.background = "hsl(25, 97%, 53%)";
-  document.querySelector("#btn1").style.color = "hsl(0, 0%, 100%)";
-  score = document.querySelector(".numbers #btn1").innerHTML;
+btn_1.onclick = function () {
+  btn_1.style.background = "hsl(25, 97%, 53%)";
+  btn_1.style.color = "hsl(0, 0%, 100%)";
+  score = btn_1.innerHTML;
   console.log(score);
   disabled();
-};
-document.querySelector("#btn2").onclick = function () {
-  document.querySelector("#btn2").style.background = "hsl(25, 97%, 53%)";
-  document.querySelector("#btn2").style.color = "hsl(0, 0%, 100%)";
-  score = document.querySelector(".numbers #btn2").innerHTML;
-  console.log(score);
-  disabled();
-};
-document.querySelector("#btn3").onclick = function () {
-  document.querySelector("#btn3").style.background = "hsl(25, 97%, 53%)";
-  document.querySelector("#btn3").style.color = "hsl(0, 0%, 100%)";
-  score = document.querySelector(".numbers #btn3").innerHTML;
-  console.log(score);
-  disabled();
-};
-document.querySelector("#btn4").onclick = function () {
-  document.querySelector("#btn4").style.background = "hsl(25, 97%, 53%)";
-  document.querySelector("#btn4").style.color = "hsl(0, 0%, 100%)";
-  score = document.querySelector(".numbers #btn4").innerHTML;
-  console.log(score);
-  disabled();
-};
-document.querySelector("#btn5").onclick = function () {
-  document.querySelector("#btn5").style.background = "hsl(25, 97%, 53%)";
-  document.querySelector("#btn5").style.color = "hsl(0, 0%, 100%)";
-  score = document.querySelector(".numbers #btn5").innerHTML;
-  console.log(score);
-  disabled();
-  feedbackGoster(score);
-};
-
-function feedbackGoster(score) {
-  let tag = `You selected ${score} out of 5`;
+  let tag = "You selected 1 out of 5";
   document.querySelector("#thanks_box #feedback").innerHTML = tag;
-}
+};
+btn_2.onclick = function () {
+  btn_2.style.background = "hsl(25, 97%, 53%)";
+  btn_2.style.color = "hsl(0, 0%, 100%)";
+  score = btn_2.innerHTML;
+  console.log(score);
+  disabled();
+  let tag = "You selected 2 out of 5";
+  document.querySelector("#thanks_box #feedback").innerHTML = tag;
+};
+btn_3.onclick = function () {
+  btn_3.style.background = "hsl(25, 97%, 53%)";
+  btn_3.style.color = "hsl(0, 0%, 100%)";
+  score = btn_3.innerHTML;
+  console.log(score);
+  disabled();
+  let tag = "You selected 3 out of 5";
+  document.querySelector("#thanks_box #feedback").innerHTML = tag;
+};
+btn_4.onclick = function () {
+  btn_4.style.background = "hsl(25, 97%, 53%)";
+  btn_4.style.color = "hsl(0, 0%, 100%)";
+  score = btn_4.innerHTML;
+  console.log(score);
+  disabled();
+  let tag = "You selected 4 out of 5";
+  document.querySelector("#thanks_box #feedback").innerHTML = tag;
+};
+btn_5.onclick = function () {
+  btn_5.style.background = "hsl(25, 97%, 53%)";
+  btn_5.style.color = "hsl(0, 0%, 100%)";
+  score = btn_5.innerHTML;
+  console.log(score);
+  disabled();
+  let tag = "You selected 5 out of 5";
+  document.querySelector("#thanks_box #feedback").innerHTML = tag;
+};
 
-btn_submit.addEventListener("click", function (score) {
+btn_submit.addEventListener("click", () => {
   thanks_box.classList.add("show");
-  feedbackGoster(score);
 });
 
-// var clicked = false;
-// document.querySelector(".numbers .btn").addEventListener("click", function () {
-//   clicked = true;
-// });
-// console.log(clicked);
-// while (!clicked) {
-//   document.getElementById("btn_submit").classList.add("disabled");
+// function feedbackGoster(score) {
+//   let tag = `You selected ${score} out of 5`;
+//   document.querySelector("#thanks_box #feedback").innerHTML = tag;
+//   console.log(tag);
 // }
+// let tag;
+// btn_submit.addEventListener("click", function (score) {
+//   thanks_box.classList.add("show");
+//   feedbackGoster(score);
+//   console.log(tag);
+// });
